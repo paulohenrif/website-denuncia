62e68be7575363888072335ff7c97c604f517cdf
paulohenrif <86567765+paulohenrif@users.noreply.github.com>
2023-07-26 15:50:37 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-Tenha calma moreno
+// <!--Importando Script Jquery-->
+/*{<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script> */}
+
+// <!--Formulário-->
+<form>
+<label for="cep">CEP</label>
+		<input id="cep" type="text" required/>
+<label for="logradouro">Logradouro</label>
+		<input id="logradouro" type="text" required/>
+<label for="numero">Número</label>
+		<input id="numero" type="text" />
+<label for="complemento">Complemento</label>
+		<input id="complemento" type="text"/>
+<label for="bairro">Bairro</label>
+		<input id="bairro" type="text" required/>
+<label for="uf">Estado</label>
+		<select id="uf">
+			<option value="AC">Acre</option>
+			<option value="AL">Alagoas</option>
+			<option value="AP">Amapá</option>
+			<option value="AM">Amazonas</option>
+			<option value="BA">Bahia</option>
+			<option value="CE">Ceará</option>
+			<option value="DF">Distrito Federal</option>
+			<option value="ES">Espírito Santo</option>
+			<option value="GO">Goiás</option>
+			<option value="MA">Maranhão</option>
+			<option value="MT">Mato Grosso</option>
+			<option value="MS">Mato Grosso do Sul</option>
+			<option value="MG">Minas Gerais</option>
+			<option value="PA">Pará</option>
+			<option value="PB">Paraíba</option>
+			<option value="PR">Paraná</option>
+			<option value="PE">Pernambuco</option>
+			<option value="PI">Piauí</option>
+			<option value="RJ">Rio de Janeiro</option>
+			<option value="RN">Rio Grande do Norte</option>
+			<option value="RS">Rio Grande do Sul</option>
+			<option value="RO">Rondônia</option>
+			<option value="RR">Roraima</option>
+			<option value="SC">Santa Catarina</option>
+			<option value="SP">São Paulo</option>
+			<option value="SE">Sergipe</option>
+			<option value="TO">Tocantins</option>
+		</select>
+</form>
