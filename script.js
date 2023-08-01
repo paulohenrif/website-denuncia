b14c5402450f699296952e61256e9c7219fb3b10
paulohenrif <86567765+paulohenrif@users.noreply.github.com>
2023-08-01 14:35:40 -0300

    .

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function clickNavbar() {
         navbar.style.display = 'block'
     }
 }
+
+
+const ulrUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+
+window.addEventListener( 'load', async () => {
+    const request = await fetch();
+})
