58cd3e041673e81bfb6dafad2cdf63779808dcc8
paulohenrif <86567765+paulohenrif@users.noreply.github.com>
2023-08-01 15:30:18 -0300

    .

--- script.js
+++ script.js
@@ -10,5 +10,11 @@ function clickNavbar() {
 const ulrUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
 
 window.addEventListener( 'load', async () => {
-    const request = await fetch();
+    const request = await fetch(ulrUF)
+    const response = await request.json()
+
+    console.log(response[26].sigla)
+    response.forEach(function(uf){
+        console.log(uf.sigla)
+    })
 })
