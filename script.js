5c2f19ed254784cb4c719218000c1f1cff74d64c
paulohenrif <86567765+paulohenrif@users.noreply.github.com>
2023-08-04 15:37:29 -0300

    .

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ function clickNavbar() {
     }
 }
 
-
 const ulrUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+
+const uf = document.getElementById("uf")
+
+uf.addEventListener('chance', async function() {
+    const urlCidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+uf.value+'/municipios'
+    const request = await fetch(urlCidades)
+    const response = await request.json()
+    console.log(response[0].nome)
+})
 
 window.addEventListener( 'load', async () => {
     const request = await fetch(ulrUF)
