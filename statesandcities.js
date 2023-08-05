f12ebabcfa6293c61af1f130389a523eedbe572e
paulohenrif <86567765+paulohenrif@users.noreply.github.com>
2023-08-05 12:56:15 -0300

    .

--- statesandcities.js
+++ statesandcities.js
@@ -1,53 +1,53 @@
 const endpointEstados = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
-        const endpointCidades = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/{{UF}}/municipios";
+const endpointCidades = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/{{UF}}/municipios";
     
-        async function buscarEstados() {
-            try {
-            const response = await fetch(endpointEstados);
-            const estados = await response.json();
-    
-            const estadoSelect = document.getElementById("estado");
-            estados.forEach((estado) => {
-                const option = document.createElement("option");
-                option.value = estado.sigla;
-                option.text = estado.nome;
-                estadoSelect.appendChild(option);
-            });
-            } catch (error) {
-            console.error("Erro ao buscar os estados:", error);
-            }
-        }
-    
-        async function buscarCidades(uf) {
-            try {
-            const response = await fetch(endpointCidades.replace("{{UF}}", uf));
-            const cidades = await response.json();
-    
-            const cidadeSelect = document.getElementById("cidade");
-            cidadeSelect.innerHTML = "<option value=''>Selecione uma cidade</option>";
-            cidades.forEach((cidade) => {
-                const option = document.createElement("option");
-                option.value = cidade.nome;
-                option.text = cidade.nome;
-                cidadeSelect.appendChild(option);
-            });
-            } catch (error) {
-            console.error("Erro ao buscar as cidades:", error);
-            }
-        }
-    
-        function atualizarCidades() {
-            const estadoSelect = document.getElementById("estado");
-            const ufSelecionado = estadoSelect.value;
-    
-            const cidadeSelect = document.getElementById("cidade");
-            cidadeSelect.innerHTML = "<option value=''>Carregando...</option>";
-    
-            if (ufSelecionado) {
-            buscarCidades(ufSelecionado);
-            } else {
-            cidadeSelect.innerHTML = "<option value=''>Selecione um estado primeiro</option>";
-            }
-        }
+    async function buscarEstados() {
+        try {
+        const response = await fetch(endpointEstados);
+        const estados = await response.json();
 
-        buscarEstados();
+        const estadoSelect = document.getElementById("estado");
+        estados.forEach((estado) => {
+            const option = document.createElement("option");
+            option.value = estado.sigla;
+            option.text = estado.nome;
+            estadoSelect.appendChild(option);
+        });
+        } catch (error) {
+        console.error("Erro ao buscar os estados:", error);
+        }
+    }
+
+    async function buscarCidades(uf) {
+        try {
+        const response = await fetch(endpointCidades.replace("{{UF}}", uf));
+        const cidades = await response.json();
+
+        const cidadeSelect = document.getElementById("cidade");
+        cidadeSelect.innerHTML = "<option value=''>Selecione uma cidade</option>";
+        cidades.forEach((cidade) => {
+            const option = document.createElement("option");
+            option.value = cidade.nome;
+            option.text = cidade.nome;
+            cidadeSelect.appendChild(option);
+        });
+        } catch (error) {
+        console.error("Erro ao buscar as cidades:", error);
+        }
+    }
+
+    function atualizarCidades() {
+        const estadoSelect = document.getElementById("estado");
+        const ufSelecionado = estadoSelect.value;
+
+        const cidadeSelect = document.getElementById("cidade");
+        cidadeSelect.innerHTML = "<option value=''>Carregando...</option>";
+
+        if (ufSelecionado) {
+        buscarCidades(ufSelecionado);
+        } else {
+        cidadeSelect.innerHTML = "<option value=''>Selecione um estado primeiro</option>";
+        }
+    }
+
+    buscarEstados();
